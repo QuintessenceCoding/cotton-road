f95548099ebe245cd81afc83ed782c498a5aef16
QuintessenceCoding <160328781+QuintessenceCoding@users.noreply.github.com>
2024-04-04 23:20:04 +0530

    Update CottonRoad.py

--- CottonRoad.py
+++ CottonRoad.py
@@ -625,7 +625,7 @@ def main():
         }
 ]
     # Base URL of the GitHub repository where images are stored
-    base_github_url = "https://raw.githubusercontent.com/QuintessenceCoding/cotton-road/main/images/"
+    base_github_url = "https://raw.githubusercontent.com/QuintessenceCoding/cotton-road-images/main/images/"
 
     # Generate photo paths for each person using the base GitHub URL
     for person in people:
